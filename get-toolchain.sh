d8d03cb577f6bb1a563eb7edb461a91a06e0c375
noreply <noreply.watcher@gmail.com>
2015-08-06 16:14:40 +0800

    update script

--- get-toolchain.sh
+++ get-toolchain.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+if [ ! -d "$HOME/.cache" ];then
+    mkdir $HOME/.cache
+else
+    echo "cache directory exist."
+fi
+
 # build for ramips/mt7620
-wget https://downloads.openwrt.org/snapshots/trunk/ramips/mt7620/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2
 
-tar xf OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2
+if [ ! -f "$HOME/.cache/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2" ];then
+    wget https://downloads.openwrt.org/snapshots/trunk/ramips/mt7620/OpenWrt-SDK-ramips-mt7620_gcc-4.8-lnaro_musl-1.1.10.Linux-x86_64.tar.bz2 -O $HOME/.cache/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2
+else
+    echo "OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2 exist, not download."
+fi
 
-mv OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64/ openwrt
+tar xf OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2 -C $HOME/openwrt
+
