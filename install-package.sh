81e0aa73ba6f0e0a1a2816bf97d2a130c122c9ca
noreply <noreply.watcher@gmail.com>
2015-08-06 17:52:13 +0800

    add new release

--- install-package.sh
+++ install-package.sh
@@ -2,6 +2,8 @@
 
 cd $HOME/openwrt
 ./scripts/feeds update -a > $HOME/feed.log 2>&1
+./scripts/feeds install libpolarssl
+
 git clone https://github.com/aa65535/openwrt-shadowvpn.git package/shadowvpn
 git clone https://github.com/shadowsocks/openwrt-shadowsocks.git package/shadowsocks-libev
 make defconfig
