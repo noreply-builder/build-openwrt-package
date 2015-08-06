e32fb0f2804cef40852467bfff2f07a3ab8e37db
noreply <noreply.watcher@gmail.com>
2015-08-06 17:36:47 +0800

    fix package

--- install-package.sh
+++ install-package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd $HOME/openwrt
-./scripts/feeds update -a
+./scripts/feeds update -a > $HOME/feed.log 2>&1
 git clone https://github.com/aa65535/openwrt-shadowvpn.git package/shadowvpn
 git clone https://github.com/shadowsocks/openwrt-shadowsocks.git package/shadowsocks-libev
 make defconfig
