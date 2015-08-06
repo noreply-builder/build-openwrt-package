24e6f45959e04dc0c6b77ed38ebd348ba05f4203
noreply <noreply.watcher@gmail.com>
2015-08-06 16:02:30 +0800

    update package

--- get-toolchain.sh
+++ get-toolchain.sh
@@ -6,4 +6,3 @@ wget https://downloads.openwrt.org/snapshots/trunk/ramips/mt7620/OpenWrt-SDK-ram
 tar xf OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2
 
 mv OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64/ openwrt
-
