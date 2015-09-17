c22ecdd022cd47e9553de8a976f958926d36127c
noreply-builder <noreply.watcher@gmail.com>
2015-09-17 17:55:27 +0800

    Update get-toolchain.sh

--- get-toolchain.sh
+++ get-toolchain.sh
@@ -8,14 +8,14 @@ fi
 
 # build for ramips/mt7620
 
-if [ ! -f "$HOME/.cache/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2" ];then
-    wget https://downloads.openwrt.org/snapshots/trunk/ramips/mt7620/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2 -O $HOME/.cache/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2
+if [ ! -f "$HOME/.cache/OpenWrt-SDK-15.05-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2" ];then
+    wget https://downloads.openwrt.org/chaos_calmer/15.05/ramips/mt7620/OpenWrt-SDK-15.05-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2 -O $HOME/.cache/OpenWrt-SDK-15.05-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2
 else
-    echo "OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2 exist, not download."
+    echo "OpenWrt-SDK-15.05-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2 exist, not download."
 fi
 
-tar xf $HOME/.cache/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64.tar.bz2 -C $HOME/
+tar xf $HOME/.cache/OpenWrt-SDK-15.05-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64.tar.bz2 -C $HOME/
 
-mv $HOME/OpenWrt-SDK-ramips-mt7620_gcc-4.8-linaro_musl-1.1.10.Linux-x86_64 $HOME/openwrt
+mv $HOME/OpenWrt-SDK-15.05-ramips-mt7620_gcc-4.8-linaro_uClibc-0.9.33.2.Linux-x86_64 $HOME/openwrt
 
 
